b87fa127f8922f2f7bf0d80cffe9657bebd73044
Neamah Al-Safarjalani <neamah@akwad.qa>
2024-09-29 04:18:36 -0400

    Add folder name to S3 upload backup - by Moh

--- akwad_frappe_fixes/native_patches.py
+++ akwad_frappe_fixes/native_patches.py
@@ -1,5 +1,6 @@
-from akwad_frappe_fixes.native_overrides import add_role_permissions_for_user_doctypes
-from frappe.core.doctype.user_type.user_type import UserType
+import frappe.utils.pdf
+from frappe.integrations.doctype.s3_backup_settings import s3_backup_settings
+from akwad_frappe_fixes.native_overrides import custom_get_pdf , custom_upload_file_to_s3
 
-
-UserType.add_role_permissions_for_user_doctypes = add_role_permissions_for_user_doctypes
+frappe.utils.pdf.get_pdf = custom_get_pdf
+s3_backup_settings.upload_file_to_s3 = custom_upload_file_to_s3
