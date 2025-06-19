097d94bdbd62aea54a238e76d59047534241f650
Alaa Hariri <alaa@akwad.qa>
2025-06-19 13:32:38 +0000

    Merging SMB Standard Features to Akwad Frappe Fixes

---
+++ akwad_frappe_fixes/akwad_frappe_fixes/doctype/smb_standard_features/smb_standard_features.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Akwad Programming and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SMB Standard Features", {
+// 	refresh(frm) {
+
+// 	},
+// });
