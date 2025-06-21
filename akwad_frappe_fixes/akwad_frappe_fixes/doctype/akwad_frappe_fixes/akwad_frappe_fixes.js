689e8f0d9d3adff681e71cd3c65b29090f409b6e
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-06-21 20:29:06 +0000

    Migrating all ERPNext changes to a new custom App

--- akwad_frappe_fixes/akwad_frappe_fixes/doctype/akwad_frappe_fixes/akwad_frappe_fixes.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Akwad Programming and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Akwad Frappe Fixes", {
-// 	refresh(frm) {
-
-// 	},
-// });
