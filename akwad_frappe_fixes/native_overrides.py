35dfb5ddd8ff9f106220c07964f48796fb1367cc
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-06-23 14:34:23 +0000

    S3-Updates by moh

--- akwad_frappe_fixes/native_overrides.py
+++ akwad_frappe_fixes/native_overrides.py
@@ -100,6 +100,7 @@ def custom_upload_file_to_s3(filename, folder, conn, bucket):
     destpath = os.path.join(f"{frappe.local.site}/{folder}", os.path.basename(filename))
     try:
         print("Uploading file:", filename)
+        frappe.log_error(title="Uploading file:" , message = str(filename))
         conn.upload_file(
             filename,
             bucket,
@@ -107,5 +108,5 @@ def custom_upload_file_to_s3(filename, folder, conn, bucket):
             ExtraArgs={"StorageClass": "GLACIER"}  
         )
     except Exception as e:
-        frappe.log_error()
+        frappe.log_error(title="Error uploading" , message = str(e))
         print("Error uploading: %s" % (e))        
