69a9d808f6ed3bc0b78d28747ab555afb6ffab08
Alaa Hariri <alaa.hariri@outlook.com>
2025-06-26 16:46:09 +0300

    Update native_patches.py

--- akwad_frappe_fixes/native_patches.py
+++ akwad_frappe_fixes/native_patches.py
@@ -1,3 +1,4 @@
+import frappe
 import frappe.utils.pdf
 from frappe.integrations.doctype.s3_backup_settings import s3_backup_settings
 from akwad_frappe_fixes.native_overrides import custom_get_pdf , custom_upload_file_to_s3
