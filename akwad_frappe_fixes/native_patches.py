5502f5c445ad33117ab404b8c70d7fcc1581fc5b
Alaa Hariri <alaa.hariri@outlook.com>
2025-06-30 15:23:04 +0300

    Update native_patches.py

--- akwad_frappe_fixes/native_patches.py
+++ akwad_frappe_fixes/native_patches.py
@@ -1,11 +1,13 @@
 try:
     import frappe
-    import frappe.utils.pdf
+#    import frappe.utils.pdf
     from frappe.integrations.doctype.s3_backup_settings import s3_backup_settings
-    from akwad_frappe_fixes.native_overrides import custom_get_pdf, custom_upload_file_to_s3
+#    from akwad_frappe_fixes.native_overrides import custom_get_pdf, custom_upload_file_to_s3
+    from akwad_frappe_fixes.native_overrides import custom_upload_file_to_s3
+
 
     # Patch only if all imports succeed
-    frappe.utils.pdf.get_pdf = custom_get_pdf
+#    frappe.utils.pdf.get_pdf = custom_get_pdf
     s3_backup_settings.upload_file_to_s3 = custom_upload_file_to_s3
 
 except ImportError as e:
