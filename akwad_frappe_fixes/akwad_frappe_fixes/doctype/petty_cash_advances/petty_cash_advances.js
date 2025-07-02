5eddf630f9dafeda1f12b04e82a8a4aea7bf800b
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-07-02 07:56:36 +0000

    Petty Cash Advances - my moh

---
+++ akwad_frappe_fixes/akwad_frappe_fixes/doctype/petty_cash_advances/petty_cash_advances.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Akwad Programming and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Petty Cash Advances", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ akwad_frappe_fixes/akwad_frappe_fixes/doctype/petty_cash_advances/test_petty_cash_advances.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Akwad Programming and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPettyCashAdvances(FrappeTestCase):
+	pass
