5e4e769dbcc93f50e6074e633d48b7825f7e3c3d
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-07-06 09:21:27 +0000

    updates

--- akwad_frappe_fixes/akwad_frappe_fixes/doctype/petty_cash_advances/petty_cash_advances.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Akwad Programming and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class PettyCashAdvances(Document):
-	pass
