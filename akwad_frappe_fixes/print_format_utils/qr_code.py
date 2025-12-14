35c2cafb3a208d6ffd065217df8763a6cfba34e3
AfnanMashilm <afnan@akwad.qa>
2025-12-14 09:42:12 +0300

    Update the QR code method

--- akwad_frappe_fixes/print_format_utils/qr_code.py
+++ akwad_frappe_fixes/print_format_utils/qr_code.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe.utils import get_url
 
-def get_qr_print_link(doctype, name, print_format=None):
+def get_qr_print_link(doctype, name, print_format):
     key = frappe.get_doc(doctype, name).get_document_share_key()
     return frappe.get_template("akwad_frappe_fixes/templates/qr_code.html").render(
         {
